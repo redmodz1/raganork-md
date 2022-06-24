4be3d74664fb733a0639c34b551bc7aa07f337f1
redmodz1 <107749935+redmodz1@users.noreply.github.com>
2022-06-24 18:41:20 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,7 +1,7 @@
-/* Copyright (C) 2022 Sourav KL11.
+/* Copyright (C) 2022 redmodz1.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-Raganork MD - Sourav KL11
+Raganork MD - redmodz1
 */
 const {
   FancyRandom,
@@ -33,12 +33,12 @@ Module({
   var buttons = [{
     urlButton: {
         displayText: 'Github',
-        url: 'https://github.com/souravkl11/raganork'
+        url: 'https://github.com/redmodz1/raganork'
     }
 }, {
   urlButton: {
     displayText: 'Contact Owner',
-    url: 'https://wa.me/'+BOT_INFO.split(";")[2]+'?text=Hey+bro'
+    url: 'https://wa.me/'+BOT_INFO.split(";")[2]+'?text=Hi+bro+bigfan'
     }
 }, {
     quickReplyButton: {
